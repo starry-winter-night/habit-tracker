12c2e31bd0573e9e6ce3fd895100f8a52311fdff
smpark <smpark7723@gmail.com>
2021-04-26 20:01:46 +0900

    React : Modify habits.jsx

--- src/components/habits.jsx
+++ src/components/habits.jsx
@@ -1,20 +1,27 @@
 import React, { Component } from 'react';
 import Habit from './habit';
+import HabitAdd from './habitAdd';
 
 class Habits extends Component {
   render() {
     return (
-      <ul>
-        {this.props.habits.map((habit) => (
-          <Habit
-            key={habit.id}
-            habit={habit}
-            onIncrement={this.props.onIncrement}
-            onDecrement={this.props.onDecrement}
-            onDelete={this.props.onDelete}
-          />
-        ))}
-      </ul>
+      <>
+        <HabitAdd onAdd={this.props.onAdd} />
+        <ul>
+          {this.props.habits.map((habit) => (
+            <Habit
+              key={habit.id}
+              habit={habit}
+              onIncrement={this.props.onIncrement}
+              onDecrement={this.props.onDecrement}
+              onDelete={this.props.onDelete}
+            />
+          ))}
+        </ul>
+        <button className="habit-reset" onClick={this.props.onReset}>
+          Reset All
+        </button>
+      </>
     );
   }
 }
