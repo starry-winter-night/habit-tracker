dc25459d4af1ee80a95ec22494c8f57226fa9948
smpark <smpark7723@gmail.com>
2021-04-28 06:09:44 +0900

    React : Optimization habit.jsx

--- src/components/habit.jsx
+++ src/components/habit.jsx
@@ -1,58 +1,39 @@
-import React, { Component } from 'react';
+import React, { useCallback, memo } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faPlusSquare,
   faMinusSquare,
   faTrash,
 } from '@fortawesome/free-solid-svg-icons';
-class Habit extends Component {
-  // componentDidMount() {
-  //   console.log(`habit: ${this.props.habit.name} mounted`)
-  // }
 
-  // componentWillUnmount() {
-  //   console.log(`habit: ${this.props.habit.name} unMounted`)
-  // }
+const Habit = memo(({ habit, onIncrement, onDecrement, onDelete }) => {
+  const handleIncrement = useCallback(() => {
+    onIncrement(habit);
+  }, [habit.count]);
 
-  handleIncrement = () => {
-    this.props.onIncrement(this.props.habit);
-  };
+  const handleDecrement = useCallback(() => {
+    onDecrement(habit);
+  }, [habit.count]);
 
-  handleDecrement = () => {
-    this.props.onDecrement(this.props.habit);
-  };
+  const handleDelete = useCallback(() => {
+    onDelete(habit);
+  }, [habit.count]);
 
-  handleDelete = () => {
-    this.props.onDelete(this.props.habit);
-  };
-  render() {
-    const { name, count } = this.props.habit;
-
-    return (
-      <li className="habit">
-        <span className="habit-name">{name}</span>
-        <span className="habit-count">{count}</span>
-        <button
-          className="habit-button habit-increase"
-          onClick={this.handleIncrement}
-        >
-          <FontAwesomeIcon icon={faPlusSquare} />
-        </button>
-        <button
-          className="habit-button habit-decrease"
-          onClick={this.handleDecrement}
-        >
-          <FontAwesomeIcon icon={faMinusSquare} />
-        </button>
-        <button
-          className="habit-button habit-delete"
-          onClick={this.handleDelete}
-        >
-          <FontAwesomeIcon icon={faTrash} />
-        </button>
-      </li>
-    );
-  }
-}
+  return (
+    <li className="habit">
+      <span className="habit-name">{habit.name}</span>
+      <span className="habit-count">{habit.count}</span>
+      <button className="habit-button habit-increase" onClick={handleIncrement}>
+        <FontAwesomeIcon icon={faPlusSquare} />
+      </button>
+      <button className="habit-button habit-decrease" onClick={handleDecrement}>
+        <FontAwesomeIcon icon={faMinusSquare} />
+      </button>
+      <button className="habit-button habit-delete" onClick={handleDelete}>
+        <FontAwesomeIcon icon={faTrash} />
+      </button>
+    </li>
+  );
+});
 
 export default Habit;
